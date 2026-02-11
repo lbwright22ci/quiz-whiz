3c095305f1caa200e9579331dd1259aeba0ebd61
lbwright22ci <lbwright22@gmail.com>
2026-02-11 16:14:51 +0000

    make reasons images on main page interactive with js

---
+++ assets/js/index.js
@@ -0,0 +1,29 @@
+document.addEventListener("DOMContentLoaded", function(){
+    
+    const detailedReason=["Each quiz consists of ten randomly generated questions.","Quizzes for everyone: from beginner to seasoned pub quiz contestant","Learn new facts to improve your general knowledge"];
+    let reasons = document.getElementsByClassName("reasons-images");
+
+    for (let j=0; j<3; j++){
+        reasons[j].addEventListener("click", function(){
+            reasons[j].removeAttribute("id");
+            reasons[j].classList.add("reasons-extra");
+            reasons[j].innerText = detailedReason[j];
+            setTimeout( () =>{     
+                reasons[j].setAttribute("id", "reason-"+(j+1));
+                reasons[j].classList.remove("reasons-extra");
+                reasons[j].innerText="";
+            }, 4000);
+        });
+        reasons[j].addEventListener("mouseenter",function(){
+            reasons[j].removeAttribute("id");
+            reasons[j].classList.add("reasons-extra");
+            reasons[j].innerText = detailedReason[j];
+            setTimeout( () =>{     
+                reasons[j].setAttribute("id", "reason-"+(j+1));
+                reasons[j].classList.remove("reasons-extra");
+                reasons[j].innerText="";
+            }, 4000);
+        });
+    }
+});
+
