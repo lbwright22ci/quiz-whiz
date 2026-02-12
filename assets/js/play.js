1e21eaab66d19c486ec81309abc633ce992ef434
lbwright22ci <lbwright22@gmail.com>
2026-02-12 12:36:18 +0000

    add instructions to the play page

--- assets/js/play.js
+++ assets/js/play.js
@@ -1,11 +1,12 @@
 document.addEventListener("DOMContentLoaded", function(){   
-    const API_KEY_easy=;
-    const API_KEY_medium=;
-    const API_KEY_difficult=; 
+    // const API_KEY_easy=;
+    // const API_KEY_medium=;
+    // const API_KEY_difficult=; 
 
     // add in a game object here
 
     document.getElementById("submit").addEventListener("click", e => startGame(e));
+    document.getElementById("info").addEventListener("click", e => toggleInstructions(e));
 
 
 });
@@ -14,3 +15,11 @@ async function startGame(e){
     const form = new FormData(document.getElementById("select-difficulty"));
 }
 
+function toggleInstructions(e){
+    var playInfo = document.getElementById("further-info");
+    if(playInfo.style.display ==="none"){
+        playInfo.style.display = "block";
+    }else{
+        playInfo.style.display = "none";
+    }
+}
