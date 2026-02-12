25087933f9420091240d9482292049af9a9b5002
lbwright22ci <lbwright22@gmail.com>
2026-02-12 10:51:32 +0000

    formatting select difficulty area of play page

--- assets/js/play.js
+++ assets/js/play.js
@@ -1,4 +1,16 @@
-document.addEventListener("DOMContentLoaded", function(){    
+document.addEventListener("DOMContentLoaded", function(){   
+    const API_KEY_easy=;
+    const API_KEY_medium=;
+    const API_KEY_difficult=; 
+
+    // add in a game object here
+
+    document.getElementById("submit").addEventListener("click", e => startGame(e));
+
 
 });
+
+async function startGame(e){
+    const form = new FormData(document.getElementById("select-difficulty"));
+}
 
