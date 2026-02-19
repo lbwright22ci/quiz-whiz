a9e19ec163292e012f5b8a49ed58f9f2a061d7ea
lbwright22ci <lbwright22@gmail.com>
2026-02-19 10:31:35 +0000

    add javascript coding for fun facts page

--- assets/js/fun-facts.js
+++ assets/js/fun-facts.js
@@ -3,13 +3,18 @@ document.addEventListener("DOMContentLoaded", function () {
   const urlToday="https://uselessfacts.jsph.pl/api/v2/facts/today?language=en";
   const urlJoke="https://v2.jokeapi.dev/joke/Pun?blacklistFlags=nsfw,religious,political,racist,sexist,explicit&type=twopart";
 
+  let joke={
+    question:"",
+    punchLine:"",
+  };
+
   document
     .getElementById("generate-random")
     .addEventListener("click", (e) => getRandomFact(e));
   document
     .getElementById("reveal-punchline")
     .addEventListener("click", (e) => jokePunchLine(e));
-  document
+
 
 async function todaysFact(){
   const response = await fetch(urlToday);
@@ -24,12 +29,22 @@ async function todaysFact(){
 todaysFact();
 
 async function getJoke(){
+  resetJoke();
   const response = await fetch(urlJoke);
   const result = await response.json();
+  joke.question = result.setup;
+  joke.punchLine = result.delivery;
 
-  document.getElementById("joke-start").innerText= result.setup;
+  document.getElementById("joke-start").innerText= joke.question;
 
   //need to add in error check
+}
+
+getJoke();
+
+function resetJoke(){
+  joke.question="";
+  joke.punchLine="";
 }
 
   async function getRandomFact(e) {
@@ -42,4 +57,16 @@ async function getJoke(){
 
     //need to add in error check
   }
+
+  function jokePunchLine(e){
+    document.getElementById("punchline-text").innerText = joke.punchLine;
+    document.getElementById("punch-line").classList.remove("hide");
+    document.getElementById("next-joke").addEventListener("click", e => newJoke(e));
+  }
+
+  function newJoke(e){
+    document.getElementById("punch-line").classList.add("hide");
+    getJoke();
+  }
+  
 });
