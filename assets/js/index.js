197b430104cd47fe674026fcf7733139a8d0d15c
lbwright22ci <lbwright22@gmail.com>
2026-02-25 15:39:04 +0000

    remove trailing semicolon from index.html

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,22 +1,24 @@
-document.addEventListener("DOMContentLoaded", function(){
-    
-    const detailedReason=["Each quiz consists of 12 randomly generated questions.","A quiz for everyone from beginner to mastermind","Learn new facts to improve your general knowledge"];
-    let reasons = document.getElementsByClassName("reasons-images");
+document.addEventListener("DOMContentLoaded", function () {
+  const detailedReason = [
+    "Each quiz consists of 12 randomly generated questions.",
+    "A quiz for everyone from beginner to mastermind",
+    "Learn new facts to improve your general knowledge",
+  ];
+  let reasons = document.getElementsByClassName("reasons-images");
 
-    for (let j=0; j<3; j++){
-        reasons[j].addEventListener("click", (e) => revealText(e, j));
-        reasons[j].addEventListener("mouseenter", (e) => revealText(e, j));
-    }
+  for (let j = 0; j < 3; j++) {
+    reasons[j].addEventListener("click", (e) => revealText(e, j));
+    reasons[j].addEventListener("mouseenter", (e) => revealText(e, j));
+  }
 
-    function revealText(e, j){
-            reasons[j].removeAttribute("id");
-            reasons[j].classList.add("reasons-extra");
-            reasons[j].innerText = detailedReason[j];
-            setTimeout( () =>{     
-                reasons[j].setAttribute("id", "reason-"+(j+1));
-                reasons[j].classList.remove("reasons-extra");
-                reasons[j].innerText="";
-            }, 4000);
-        };
-});
-
+  function revealText(e, j) {
+    reasons[j].removeAttribute("id");
+    reasons[j].classList.add("reasons-extra");
+    reasons[j].innerText = detailedReason[j];
+    setTimeout(() => {
+      reasons[j].setAttribute("id", "reason-" + (j + 1));
+      reasons[j].classList.remove("reasons-extra");
+      reasons[j].innerText = "";
+    }, 4000);
+  }
+})
